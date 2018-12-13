b0839ca487d80d6630821644ccffdb9ba4ae4b7f
TKJ-X <malicha>
2018-12-13 11:19:12 +0700

    merapikan kode di app.js

--- src/App.js
+++ src/App.js
@@ -28,20 +28,9 @@ class App extends Component{
         {isPage === "Mondok" && (<Mondok/>)}
         {isPage === "Kitab" && (<Kitab/>)}
        
-
-        {/* {isPage === "Home" ? (<Home />) : isPage === "Mondok" ? (<Mondok />): isPage === "Kitab" ? (<Kitab />) : (<h1> Halaman tidak ditemukan </h1>)} */}
      </div>
    )
-  //  if(isPage === "Home"){
-  //    return(<div><NavBar changePage={this.changePage}/><Home/></div>)
-  //  }else if(isPage === "Kitab"){
-  //    return(<div><NavBar changePage={this.changePage}/><Kitab/></div>)
-  //  }else if(isPage === "Kajian"){
-  //    return(<div><NavBar changePage={this.changePage}/><Kajian/></div>)
-  //  }else if(isPage === "Mondok"){
-  //    return(<div><NavBar changePage={this.changePage}/><Mondok/></div>)
-  //  }
-  
+ 
   }
 }
 
